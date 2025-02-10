c0372b0d4761b423a3f0d4ab378b76a46f062fc6
Atin Bainada <atin4@proton.me>
2025-02-10 21:46:09 +0530

    use nix-shell to run cmds

--- src/main.rs
+++ src/main.rs
@@ -20,19 +20,32 @@ enum Commands {
     F,
 }
 
+const GIT_ADD_ALL: &str = "git add -A";
+const NVD_DIFF: &str = "nvd diff $(ls -d1v /nix/var/nix/profiles/system-*-link | tail -n 2)";
+const BUILD_SYSTEM: &str = "nix build .#nixosConfigurations.nixos.config.system.build.toplevel";
+const UPDATE_FLAKES: &str = "nix flake update";
+const NIXOS_REBUILD: &str = "sudo nixos-rebuild switch --flake .#nixos";
+
 fn main() {
     let cli = Cli::parse();
+    let mut cmds = vec![];
 
     match &cli.command {
         Some(Commands::Su) => {
-            println!("Su cmd");
+            cmds = [GIT_ADD_ALL, UPDATE_FLAKES, BUILD_SYSTEM, NIXOS_REBUILD].to_vec();
+            git_add_all();
+            update_flakes();
+            build_system();
+            nixos_rebuild();
         }
         Some(Commands::D) => {
-            nvd_diff();
+            cmds = [NVD_DIFF].to_vec();
         }
         _ => {
             git_add_all();
             build_system();
         }
     }
+
+    run_all(&cmds);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -2,43 +2,60 @@ use anyhow::{bail, Context, Ok, Result};
 use std::process::Command;
 
 pub fn git_add_all() -> Result<()> {
-    run_cmd("git", &["add", "-A"], "git add -A")?;
+    run_cmd("git add -A")?;
     Ok(())
 }
 
 pub fn nvd_diff() -> Result<()> {
-    run_cmd(
-        "bash",
-        &[
-            "-c",
-            "nvd diff $(ls -d1v /nix/var/nix/profiles/system-*-link | tail -n 2)",
-        ],
-        "nvd diff",
-    )?;
+    run_cmd("nvd diff $(ls -d1v /nix/var/nix/profiles/system-*-link | tail -n 2)")?;
     Ok(())
 }
 
 pub fn build_system() -> Result<()> {
-    run_cmd(
-        "nix",
-        &[
-            "build",
-            ".#nixosConfigurations.nixos.config.system.build.toplevel",
-        ],
-        "`nix build`",
-    )?;
+    run_cmd("nix build .#nixosConfigurations.nixos.config.system.build.toplevel")?;
     Ok(())
 }
 
-pub fn run_cmd(cmd: &str, args: &[&str], ctx_msg: &str) -> Result<()> {
-    let status = Command::new(cmd)
+pub fn update_flakes() -> Result<()> {
+    run_cmd("nix flake update")?;
+    Ok(())
+}
+
+pub fn nixos_rebuild() -> Result<()> {
+    run_cmd("sudo nixos_rebuild switch --flake .#nixos")?;
+    Ok(())
+}
+
+// TODO: add pkgs to nix-shell
+pub fn run_all(cmds: &[&str]) -> Result<()> {
+    let script = cmds.join(" && ");
+    let inner_cmd = format!("bash -c 'set -x; {}'", script);
+
+    let args = ["-p", "nvd", "--run", inner_cmd.as_str()];
+    let status = Command::new("nix-shell")
+        .args(args)
+        .current_dir("/home/atin/.dotfiles/")
+        .status()
+        .with_context(|| format!("Failed to execute."))?;
+
+    if !status.success() {
+        bail!("something failed: {}", status);
+    }
+
+    Ok(())
+}
+
+pub fn run_cmd(cmd: &str) -> Result<()> {
+    let mut args = vec!["-c", "-x"];
+    args.push(cmd);
+    let status = Command::new("bash")
         .args(args)
         .current_dir("/home/atin/.dotfiles/")
         .status()
         .with_context(|| format!("Failed to execute {}", cmd))?;
 
     if !status.success() {
-        bail!("{} failed: {}", ctx_msg, status);
+        bail!("{} failed: {}", cmd, status);
     }
 
     Ok(())
